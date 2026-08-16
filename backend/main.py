906f011e688f50cf76c8f961251366acf11dd718
SRIMANI ANKAM <srimaniankam@gmail.com>
2026-08-16 23:10:22 +0530

    Fix CORS for deployed frontend

--- backend/main.py
+++ backend/main.py
@@ -45,7 +45,9 @@ app.add_middleware(
 
         "http://localhost:5173",
 
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+
+        "https://railai-5wo7.onrender.com"
 
     ],
 
